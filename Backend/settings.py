69b4b09daca3067736a0d6be3f0177a2a0b88e84
damiemmy <damisaemmanuel778@gmail.com>
2025-09-01 22:19:51 -0700

    django-ecommerce-backend api deployment on Render

--- Backend/settings.py
+++ Backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-2n*u9eco7a4lhy^rsecjsd0fk(l)z$_r*3h36771)@65wteq0y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-ecommerce-backend-qyjh.onrender.com","local_host","127.0.0.1"]
 
 
 # Application definition
