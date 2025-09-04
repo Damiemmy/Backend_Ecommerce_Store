852c27d1b801a33d9c94fb923d2cce49a6fd848d
damiemmy <damisaemmanuel778@gmail.com>
2025-09-04 16:27:08 -0700

    Backend Final deployment on Render,added Live url to allowed host

--- Backend/settings.py
+++ Backend/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = [
     "django-ecommerce-backend-qyjh.onrender.com",  # your Render backend
     "localhost",   # correct spell
     "127.0.0.1",
+    "backend-ecommerce-store-1.onrender.com",
     "buyit-backend-iml8.onrender.com",   # local loopback
     "[::1]"        # IPv6 localhost (optional but useful)
 ]
